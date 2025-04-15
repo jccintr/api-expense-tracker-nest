c2e2ecab258d73087313cd3a6da3262ace25ceab
Julio Cesar <jccintr@gmail.com>
2025-04-15 16:59:58 -0300

    criado transactions resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,12 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { AccountsModule } from './accounts/accounts.module';
 import { CategoriesModule } from './categories/categories.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
   imports: [DatabaseModule, UsersModule, AuthModule,ConfigModule.forRoot({
     isGlobal: true, // Torna o módulo disponível globalmente
-  }), AccountsModule, CategoriesModule,],
+  }), AccountsModule, CategoriesModule, TransactionsModule,],
   controllers: [AppController],
   providers: [AppService],
 })
