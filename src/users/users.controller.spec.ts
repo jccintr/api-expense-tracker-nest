e661f4716294343c50bcf616068eae224bd6c9e5
Julio Cesar <jccintr@gmail.com>
2025-04-24 23:07:38 -0300

    implementado user controller tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -13,6 +13,16 @@ describe('UsersController', () => {
   let controller: UsersController;
   let service: UsersService;
 
+
+  var mockUsersService: {
+    create: jest.Mock,
+    findAllPaginated: jest.Mock,
+    findOne: jest.Mock,
+    update: jest.Mock,
+    remove: jest.Mock,
+  };
+
+  /*
   const mockUsersService = {
     create: jest.fn(dto => ({ id: 1, ...dto })),
     findAllPaginated: jest.fn(() => ({
@@ -26,6 +36,7 @@ describe('UsersController', () => {
     remove: jest.fn(id => ({ message: 'Usuário removido com sucesso' })),
     
   };
+*/
 
   // Mock simples para AuthGuard
   const mockAuthGuard = {
@@ -33,6 +44,19 @@ describe('UsersController', () => {
   };
 
   beforeEach(async () => {
+    mockUsersService = {
+      create: jest.fn(dto => ({ id: 1, ...dto })),
+      findAllPaginated: jest.fn(() => ({
+        data: [],
+        total: 0,
+        page: 1,
+        lastPage: 1,
+      })),
+      findOne: jest.fn().mockResolvedValue({ id: 1, name: 'Test', email: 'test@example.com' }),
+      update: jest.fn().mockResolvedValue({ id: 1, name: 'Updated', email: 'test@example.com' }),
+      remove: jest.fn().mockResolvedValue({ message: 'Usuário removido com sucesso' }),
+    };
+    
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [
@@ -75,13 +99,31 @@ describe('UsersController', () => {
           const result = await controller.findOne('1');
           expect(result).toEqual({ id: 1, name: 'Test', email: 'test@example.com' });
         });
+
+        it('deve lançar NotFoundException se o id não existir', async () => {
+          const id = 999;
+          jest.spyOn(mockUsersService, 'findOne').mockRejectedValue(new NotFoundException(`Ressource not found. ID ${id}`));
+        
+          await expect(controller.findOne(id.toString())).rejects.toThrow(NotFoundException);
+          expect(mockUsersService.findOne).toHaveBeenCalledWith(id);
+        });
   });
 
 
   describe('update', () => {
       it('deve atualizar um usuário se o id existir', async () => {
         const dto: UpdateUserDto = { name: 'Updated Name' };
-        expect(await controller.update('1', dto)).toEqual({ id: 1, ...dto });
+        expect(await controller.update('1', dto)).toEqual({ id: 1, name: 'Updated', email: 'test@example.com' });
+      });
+
+      it('deve lançar NotFoundException se o id não existir', async () => {
+        const id = 1; 
+        const dto = { name: 'Updated' };
+      
+        jest.spyOn(mockUsersService, 'update').mockRejectedValue(new NotFoundException(`Ressource not found. ID ${id}`));
+      
+        await expect(controller.update(id.toString(), dto)).rejects.toThrow(NotFoundException);
+        expect(mockUsersService.update).toHaveBeenCalledWith(id, dto);
       });
   });
 
@@ -90,6 +132,15 @@ describe('UsersController', () => {
   describe('remove', () => {
         it('deve remover um usuário se o id existir', async () => {
           expect(await controller.remove('1')).toEqual({ message: 'Usuário removido com sucesso' });
+        });
+        it('deve lançar NotFoundException se o id não existir', async () => {
+          const id = 1; 
+        
+        
+          jest.spyOn(mockUsersService, 'remove').mockRejectedValue(new NotFoundException(`Ressource not found. ID ${id}`));
+        
+          await expect(controller.remove(id.toString())).rejects.toThrow(NotFoundException);
+          expect(mockUsersService.remove).toHaveBeenCalledWith(id,);
         });
    });
 
